4423c4e388f28ad377bbc352d87dc6c54e5cc9ec
Ziex <andra778@yahoo.com>
2025-05-19 08:16:55 +0700

    Need edit on bit since custom Script didnt reaon on repo !! Fak

--- src/server/scripts/Vip/Vip_SC.cpp
+++ src/server/scripts/Vip/Vip_SC.cpp
@@ -1,6 +1,5 @@
 /*
  * This file is part of the WarheadCore Project. See AUTHORS file for Copyright information
- *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Affero General Public License as published by the
  * Free Software Foundation; either version 3 of the License, or (at your
